0673941d1fd6c20dbb07f56604419233011bf946
Ayush Sachan <ayushsachan162@gmail.com>
2026-03-20 17:28:00 +0530

    wallet

--- app/transaction-miner.js
+++ app/transaction-miner.js
@@ -1,3 +1,5 @@
+const Transaction = require('../wallet/transaction');
+
 class TransactionMiner {
   constructor({ blockchain, transactionPool, wallet, pubsub }) {
     this.blockchain = blockchain;
@@ -8,14 +10,17 @@ class TransactionMiner {
 
   mineTransactions() {
     // get the transaction pool's valid transactions
-
+    const validTransactions = this.transactionPool.validTransactions();
     // generate the miner's reward
-
-    // add a block consisting of these transactions to the blockchain
-
-    // broadcast the updated blockchain
-
-    // clear the pool
+    validTransactions.push(Transaction.rewardTransaction({ minerWallet: this.wallet }));
+     // add a block consisting of these transactions to the blockchain
+    this.blockchain.addBlock({ data: validTransactions });
+     // broadcast the updated blockchain
+    this.pubsub.broadcastChain();
+     // clear the pool
+    this.transactionPool.clearBlockchainTransactions({
+      chain: this.blockchain.chain
+    });
   }
 }
 
