7713654721eccb38cbbca7ce06fdfcd552e24b49
Ayush Sachan <ayushsachan162@gmail.com>
2026-03-20 17:51:03 +0530

    wallet balanc

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -67,6 +67,24 @@ describe('Wallet', () => {
                 expect(transaction.outputMap[recipient]).toEqual(amount);
             });
         });
+
+        describe ('and chain is passed', () => {
+            it('calls `Wallet.calculateBalance`', () => {
+                const calculateBalanceMock = jest.fn();
+                const originalCalculateBalance = Wallet.calculateBalance;
+                Wallet.calculateBalance = calculateBalanceMock;
+
+                wallet.createTransaction({ 
+                    recipient: 'foo', 
+                    amount: 10, 
+                    chain: new Blockchain().chain 
+                });
+                expect(calculateBalanceMock).toHaveBeenCalled();
+
+                Wallet.calculateBalance = originalCalculateBalance
+            });
+        });
+
     });
 
     describe ('calculateBalance()', () => {
